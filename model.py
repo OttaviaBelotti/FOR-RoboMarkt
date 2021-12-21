e3442dfb68eca097da92e72b8fdf39c3cd8a6a32
AlessioBraccini <alessio.braccini@mail.polimi.it>
2021-12-21 13:56:59 +0100

    Add parser to read dat file and put in variables

--- model.py
+++ model.py
@@ -2,14 +2,33 @@ from itertools import product
 from sys import stdout as out
 from mip import Model, xsum, minimize, BINARY
 
-location = [[]]
-distance = [[]]
+# Open files dat and read file
+
+datFile = "datasets/minimart-I-50.dat"
+
+datContent = [i.strip().split() for i in open(datFile).readlines()]
+
+param = []
+for i in datContent[:5]:
+    param.append(int(i[3]))
+
+# Variable declaration
+
+location = datContent[7:len(datContent)-1]
+for i in location:
+    for j in range(len(i)):
+        i[j] = int(i[j])
+
+n = param[0]
+range_param = param[1]
+vc = param[2]
+fc = param[3]
+capacity = param[4]
+
+# distance = [[]]
 truck = []
-n = 50
-range_param = 7
-vc = 1
-fc = 50
-capacity = 10
+
+# print(n, range_param, vc, fc, capacity, location)
 
 N = set(range(len(location)))
 T = set(range(len(truck)))
@@ -32,6 +51,4 @@ for i in N:
     model += xsum(x[i][j] * y[i] for j in T) == 1
 
 #model += xsum(y[j] for (j in N, distance[i,j]<range_param)) >= 1
-
-
 
